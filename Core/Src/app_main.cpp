a6de5945ec789c6f5bad9d71feaff6a99d82399f
Edwin Koch <edwin.koch@ost.ch>
2020-10-06 09:54:44 +0200

    added blinki example

--- Core/Src/app_main.cpp
+++ Core/Src/app_main.cpp
@@ -1,10 +1,20 @@
 #include "app_main.h"
 
+#include "main.h"
+#include "stm32f4xx_hal.h"
+#include "cmsis_os.h"
+
 void appMain(void* args)
 {
 
 	while(1)
 	{
+		HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_SET);
 
+		osDelay(100);
+
+		HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_RESET);
+
+		osDelay(100);
 	}
 }
